bc310fbb62b2f0cedfcd859276df8fea48012110
TheSeaToad <me@theseatoad.com>
2022-07-23 14:59:34 -0700

    Unit testing
    Additionally renamed scissor to scissors to keep things consistent.

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ struct Cli {
 enum Move {
     Rock,
     Paper,
-    Scissor
+    Scissors
 }
 
 impl Distribution<Move> for Standard {
@@ -29,7 +29,7 @@ impl Distribution<Move> for Standard {
         match rng.gen_range(0..=2) {
             0 => Move::Rock,
             1 => Move::Paper,
-            _ => Move::Scissor,
+            _ => Move::Scissors,
         }
     }
 }
@@ -39,13 +39,13 @@ impl PartialOrd for Move {
         match (self, other) {
             (Move::Rock, Move::Rock) => Some(Ordering::Equal),
             (Move::Rock, Move::Paper) => Some(Ordering::Less),
-            (Move::Rock, Move::Scissor) => Some(Ordering::Greater),
+            (Move::Rock, Move::Scissors) => Some(Ordering::Greater),
             (Move::Paper, Move::Rock) => Some(Ordering::Greater),
             (Move::Paper, Move::Paper) => Some(Ordering::Equal),
-            (Move::Paper, Move::Scissor) => Some(Ordering::Less),
-            (Move::Scissor, Move::Rock) => Some(Ordering::Less),
-            (Move::Scissor, Move::Paper) => Some(Ordering::Greater),
-            (Move::Scissor, Move::Scissor) => Some(Ordering::Equal),
+            (Move::Paper, Move::Scissors) => Some(Ordering::Less),
+            (Move::Scissors, Move::Rock) => Some(Ordering::Less),
+            (Move::Scissors, Move::Paper) => Some(Ordering::Greater),
+            (Move::Scissors, Move::Scissors) => Some(Ordering::Equal),
         }
     }
 }
@@ -57,12 +57,12 @@ impl FromStr for Move {
         match s.to_ascii_lowercase().as_str() {
             "rock" => Ok(Move::Rock),
             "paper" => Ok(Move::Paper),
-            "scissors" => Ok(Move::Scissor),
+            "scissors" => Ok(Move::Scissors),
             _ => Err(clap::ErrorKind::InvalidValue),
         }
     }
 }
-
+#[derive(Debug, PartialEq)]
 enum GameResult {
     UserWin,
     OpponentWin,
@@ -110,4 +110,123 @@ fn calculate_winner(user: Move, opponent:Move) -> GameResult {
     } else {
         return GameResult::OpponentWin
     }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+        /** ==== Partial ordering ==== **/
+        /* ==== Rocks ==== */
+
+    #[test]
+    fn test_partial_ordering_rock_rock(){
+        assert_eq!(Move::Rock, Move::Rock)
+    }
+
+    #[test]
+    fn test_partial_ordering_rock_paper(){
+        assert_eq!((Move::Rock < Move::Paper), true)
+    }
+
+    #[test]
+    fn test_partial_ordering_rock_scissors(){
+        assert_eq!((Move::Rock > Move::Scissors), true)
+    }
+
+        /* ==== Paper ==== */
+
+    #[test]
+    fn test_partial_ordering_paper_rock(){
+        assert_eq!((Move::Paper > Move::Rock), true)
+    }
+
+    #[test]
+    fn test_partial_ordering_paper_paper(){
+        assert_eq!(Move::Paper, Move::Paper)
+    }
+
+    #[test]
+    fn test_partial_ordering_paper_scissors(){
+        assert_eq!((Move::Paper < Move::Scissors), true)
+    }
+
+        /* ==== Scissors ==== */
+
+    #[test]
+    fn test_partial_ordering_scissors_rock(){
+        assert_eq!((Move::Scissors < Move::Rock), true)
+    }
+
+    #[test]
+    fn test_partial_ordering_scissors_paper(){
+        assert_eq!((Move::Scissors > Move::Paper), true)
+    }
+
+    #[test]
+    fn test_partial_ordering_scissors_scissors(){
+        assert_eq!(Move::Scissors, Move::Scissors)
+    }
+
+        /** ==== Calculate winner==== **/
+
+    #[test]
+    fn test_calculate_winner_userwin(){
+        assert_eq!(calculate_winner(Move::Rock, Move::Scissors), GameResult::UserWin)
+    }
+
+    #[test]
+    fn test_calculate_winner_tie(){
+        assert_eq!(calculate_winner(Move::Rock, Move::Rock), GameResult::Tie)
+    }
+
+    #[test]
+    fn test_calculate_winner_opponentwin(){
+        assert_eq!(calculate_winner(Move::Rock, Move::Paper), GameResult::OpponentWin)
+    }
+
+        /** ==== FromStr ==== **/
+
+    #[test]
+    fn test_fromstr_rock(){
+        assert_eq!(Move::from_str("rock"), Ok(Move::Rock))
+    }
+
+    #[test]
+    fn test_fromstr_paper(){
+        assert_eq!(Move::from_str("paper"), Ok(Move::Paper))
+    }
+
+    #[test]
+    fn test_fromstr_scissors(){
+        assert_eq!(Move::from_str("scissors"), Ok(Move::Scissors))
+    }
+
+    #[test]
+    fn test_fromstr_error(){
+        assert_eq!(Move::from_str("notreal"), Err(clap::ErrorKind::InvalidValue))
+    }
+
+    /** ==== Seeded Random Completeness ==== **/
+    
+    #[test]
+    fn test_distribution_rock(){
+        let mut seed = StdRng::seed_from_u64(2);
+        let seeded_move : Move = seed.gen();
+        assert_eq!(seeded_move, Move::Rock)
+    }
+    
+    #[test]
+    fn test_distribution_paper(){
+        let mut seed = StdRng::seed_from_u64(7);
+        let seeded_move : Move = seed.gen();
+        assert_eq!(seeded_move, Move::Paper)
+    }
+
+    #[test]
+    fn test_distribution_scissors(){
+        let mut seed = StdRng::seed_from_u64(1);
+        let seeded_move : Move = seed.gen();
+        assert_eq!(seeded_move, Move::Scissors)
+    }
+
 }
